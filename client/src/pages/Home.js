44e19ece3ccb9285222624d2d94f916f3ca1124f
tikak <tika.kandel.06@gmail.com>
2021-10-21 23:13:01 +1100

    server setup

---
+++ client/src/pages/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import { Container } from "../components/Container";
+import { H2 } from '../components/Text';
+
+const Home = () => {
+  return (
+    <Container alignContent="center">
+      <H2>Hello world</H2>
+    </Container>
+  );
+};
+
+export default Home;
